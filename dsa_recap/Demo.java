8adf62ba5ff3fdbd5064aec39d6617fc0b8f0994
edielam <edguard738@gmail.com>
2024-02-10 22:38:39 -0500

    dsa recap

--- dsa_recap/Demo.java
+++ dsa_recap/Demo.java
@@ -1,11 +1,12 @@
 public class Demo {
     public static void main(String[] args) {
         int nums[] = {4,6,11,24,25,33,67,96};
-        int target = 4;
+        int target = 7;
 
-        int result = BinarySearch(nums, target);
-        if(result != -1){
-            System.out.println("Answer is :"+ result);
+        int result1 = BinarySearch(nums, target);
+        int result2 = RecurBinarySearch(nums, target,0, nums.length-1);
+        if(result2 != -1){
+            System.out.println("Answer is :"+ result2);
         } else {
             System.out.println("Not found");
         }
@@ -32,6 +33,19 @@ public class Demo {
                 end = mid - 1;
             } else
                 start = mid + 1;
+        }
+        return -1;
+    }
+    public static int RecurBinarySearch(int num[], int target, int start, int end){
+        if (start <= end){
+            int mid = (start + end)/2;
+            if(num[mid]==target){
+                return mid;
+            }
+            else if(num[mid]> target){
+                return RecurBinarySearch(num, target, start, end = mid - 1);
+            } else
+                return RecurBinarySearch(num, target, start = mid + 1, end);
         }
         return -1;
     }
