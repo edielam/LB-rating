d2f0a47e7d61c2c6d7ee35690d659a1c6ed252ed
edielam <edguard738@gmail.com>
2024-02-11 00:25:04 -0500

    dsa recap

--- dsa_recap/Demo2.java
+++ dsa_recap/Demo2.java
@@ -7,12 +7,27 @@ public class Demo2 {
         for(int num1 : nums){
             System.out.print(num1 +" ");
         }
-        int result[] = BubbleSort(nums); //Put it here, so the original array is only modified at this point
+        int result[] = SelectionSort(nums); //Put it here, so the original array is only modified at this point
         System.out.println();
         System.out.println("After sorting");
         for(int num2 : result){
             System.out.print(num2 +" ");
         }
+    }
+    public static int[] SelectionSort(int num[]){
+        int temp = 0;
+        for(int i = num.length-1; i>=0; i--){
+            int maxIndex = 0; //select!!
+            for(int j = 1; j<= i; j++){
+                if(num[j]>num[maxIndex]){ //select!!
+                    maxIndex = j;
+                }
+            }
+            temp = num[i];
+            num[i] = num[maxIndex];
+            num[maxIndex] = temp;
+        }
+        return num;
     }
     public static int[] BubbleSort(int num[]){
         int temp = 0;
