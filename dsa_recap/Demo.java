016283e41047c6878cb841bb934bf3d722413878
edielam <edguard738@gmail.com>
2024-02-10 21:53:27 -0500

    dsa recap

--- dsa_recap/Demo.java
+++ dsa_recap/Demo.java
@@ -4,8 +4,18 @@ public class Demo {
         int target = 11;
 
         int result = LinearSearch(nums, target);
+        if(result != -1){
+            System.out.println("Answer is :"+ result);
+        } else {
+            System.out.println("Not found");
+        }
     }
     public static int LinearSearch(int num[], int target){
+        for (int i = 0; i<num.length; i++ ){
+            if (num[i] == target){
+                return i;
+            }
+        }
         return -1;
     }
 }
