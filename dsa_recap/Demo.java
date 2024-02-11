e125cbc6c75888dd10b3da60af32c861d4c3c77c
edielam <edguard738@gmail.com>
2024-02-10 21:48:51 -0500

    dsa recap

--- dsa_recap/Demo.java
+++ dsa_recap/Demo.java
@@ -1,5 +1,11 @@
 public class Demo {
     public static void main(String[] args) {
-        
+        int nums[] = {4,6,11,24,25};
+        int target = 11;
+
+        int result = LinearSearch(nums, target);
+    }
+    public static int LinearSearch(int num[], int target){
+        return -1;
     }
 }
